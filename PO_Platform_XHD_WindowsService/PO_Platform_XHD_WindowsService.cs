8614095d78afda39b29e28ad136b1bf9cda7051e
prayerever <2414290411@qq.com>
2020-01-02 09:21:07 +0800

    Add files via upload

---
+++ PO_Platform_XHD_WindowsService/PO_Platform_XHD_WindowsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_Platform_XHD_WindowsService
+{
+    public partial class PO_Platform_XHD_WindowsService : ServiceBase
+    {
+        public PO_Platform_XHD_WindowsService()
+        {
+            string s = "hello";
+            Console.WriteLine(s);
+        }
+
+    }
+}
